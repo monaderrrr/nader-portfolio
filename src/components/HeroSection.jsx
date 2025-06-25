5c3d848c1e8c986db11709280bbf1542523e6c87
mohamednaderr0 <mnader200444@gmail.com>
2025-06-25 14:57:57 +0300

    first commit

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,32 @@
+
+
+export const HeroSection =() => {
+    return (
+        <section
+            id ="hero" 
+        className="relative min-h-screen flex flex-col items-center justify-center px-4"
+        >
+            <div className="container max-w-4xl mx-auto text-center z-10">
+                <div className="space-y-6">
+                    <h1 className="text-4xl md:text-6xl font-bold tracking-tight">
+                        <span className="opacity-0 animate-fade-in">Hi, I'm </span>
+                        <span className="text-sky-400 opacity-0 animate-fade-in-delay-1">
+                            {""}
+                            Mohamed
+                            </span>
+                        <span className="text-gradient ml-2 opacity-0 animate-fade-in-delay-2">
+                            {""}
+                            Nader
+                            </span>
+                    </h1>
+
+                    <p className="text-lg md:text-xl text-muted-foreground max-w-2xl mx-auto opacity-0 animate-fade-in-delay-3">
+                    I develop stellar web experiences with cutting-edge tools. Specializing in front-end design, I craft interfaces that are fast and intuitive.
+                    </p>
+
+                </div>
+
+            </div>
+        </section>
+    );
+};
