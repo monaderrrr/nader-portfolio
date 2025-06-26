a02a93b8b3c860b4d7d7328cda5fded269375413
mohamednaderr0 <mnader200444@gmail.com>
2025-06-26 14:21:11 +0300

    Quick update

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -20,10 +20,15 @@ export const HeroSection =() => {
                             </span>
                     </h1>
 
-                    <p className="text-lg md:text-xl text-muted-foreground max-w-2xl mx-auto opacity-0 animate-fade-in-delay-3">
+                    <p className="text-lg md:text-xl text-muted-foreground max-2-2xl mx-auto opacity-0 animate-fade-in-delay-3">
                     I develop stellar web experiences with cutting-edge tools. Specializing in front-end design, I craft interfaces that are fast and intuitive.
                     </p>
 
+                    <div className="pt-4 opacity-0 animate-fade-in-delay-4">
+                        <a href="#projects" className="cosmic-button">
+                            View My Work
+                        </a>
+                    </div>
                 </div>
 
             </div>
